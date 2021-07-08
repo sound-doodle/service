c5096264abca85721390c610d8c1f9b76c70a02a
Lee Scroggins <lscrggns@yahoo.com>
2021-07-08 09:11:47 -0600

    Initial commit.

---
+++ src/test/java/edu/cnm/deepdive/sounddoodleservice/SoundDoodleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.sounddoodleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundDoodleServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
