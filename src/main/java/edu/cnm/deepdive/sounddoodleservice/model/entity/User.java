c177e1770922a2f18dc156e7bd21ced6f2404aab
Lee Scroggins <lscrggns@yahoo.com>
2021-07-19 09:50:51 -0600

    Updated User model entity with date created and connected fields with getters and or setters.

--- src/main/java/edu/cnm/deepdive/sounddoodleservice/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/sounddoodleservice/model/entity/User.java
@@ -1,14 +1,26 @@
 package edu.cnm.deepdive.sounddoodleservice.model.entity;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.lang.NonNull;
 
 @Entity
-@Table(name = "user_profile")
+@Table(
+    name = "user_profile",
+    indexes = {
+        @Index(columnList = "created"),
+        @Index(columnList = "connected")
+    }
+)
 public class User {
 
   @Id
@@ -19,26 +31,57 @@ public class User {
   @Column(nullable = false, unique = true)
   private String userName;
 
+  @NonNull
   @Column(nullable = false, updatable = false, unique = true)
   private String oauthKey;
 
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date created;
+
+  @NonNull
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date connected;
+
+  @NonNull
   public Long getId() {
     return id;
   }
 
+  @NonNull
   public String getUserName() {
     return userName;
   }
 
+  @NonNull
   public void setUserName(String userName) {
     this.userName = userName;
   }
 
+  @NonNull
   public String getOauthKey() {
     return oauthKey;
   }
 
+  @NonNull
   public void setOauthKey(String oauthKey) {
     this.oauthKey = oauthKey;
+  }
+
+  @NonNull
+  public Date getCreated() {
+    return created;
+  }
+
+  @NonNull
+  public Date getConnected() {
+    return connected;
+  }
+
+  public void setConnected(@NonNull Date connected) {
+    this.connected = connected;
   }
 }
