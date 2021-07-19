69870d16e96c4ebc4296aa9148c0e90a610ac0ed
Lee Scroggins <lscrggns@yahoo.com>
2021-07-19 15:02:18 -0600

    Implemented oauthkey and user create.

--- src/main/java/edu/cnm/deepdive/sounddoodleservice/model/service/UserService.java
+++ src/main/java/edu/cnm/deepdive/sounddoodleservice/model/service/UserService.java
@@ -2,11 +2,17 @@ package edu.cnm.deepdive.sounddoodleservice.model.service;
 
 import edu.cnm.deepdive.sounddoodleservice.model.dao.UserRepository;
 import edu.cnm.deepdive.sounddoodleservice.model.entity.User;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements Converter<Jwt, UsernamePasswordAuthenticationToken> {
 
   private final UserRepository repository;
 
@@ -30,5 +36,10 @@ public class UserService {
     });
   }
 
-
+  @Override
+  public UsernamePasswordAuthenticationToken convert(Jwt jwt) {
+    Collection<SimpleGrantedAuthority> grants = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    return new UsernamePasswordAuthenticationToken(
+        getOrCreate(jwt.getSubject(), jwt.getClaim("name")), jwt.getTokenValue(), grants);
+      }
 }
